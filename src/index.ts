0a2514f76c86b3d6b197c6fdd687829ab7c1c3cf
leesh08178 <leesh08178@gmail.com>
2022-05-24 11:33:26 +0900

    서버 동기화 수정

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@ import { Message, OnMessage, ServiceMessage, RefreshMessage, clientsByRoomType, 
 import ChannelServiceManager from "./channelService";
 const { createClient } = require('redis');
 const { createAdapter } = require('@socket.io/redis-adapter');
-const channels:Map<string, Map<string, string>> = new Map<string, Map<string, string>>();
-const clientRoom:Map<string, Map<string, string>> = new Map<string, Map<string, string>>();
 
 function run(port: number = 3000): void {
     const app = express();
@@ -29,18 +27,6 @@ function run(port: number = 3000): void {
         res.sendFile('index.html', { root: __dirname + '/html/'});
     });
 
-    io.on('onJoinRoom', (eventMessage: EventMessage) => {
-        for(const room of eventMessage.rooms) {
-            joinChannel(eventMessage.id, 'Room:'+room);
-        }
-    });
-
-    io.on('onLeaveRoom', (eventMessage: EventMessage) => {
-        for(const room of eventMessage.rooms) {
-            leaveChannel(eventMessage.id, 'Room:'+room);
-        }
-    });
-
     io.on('onCreateService', (serviceMessage: ServiceMessage) => {
         channelServiceManager.CreateService(serviceMessage.name, serviceMessage.nsp);
         io.emit('onCreateServiceClient', serviceMessage.name);
@@ -52,10 +38,12 @@ function run(port: number = 3000): void {
     });
 
     io.on('connection', (socket: any) => {
-        socket.on('ready', () => {
+        socket.on('ready', async () => {
             socket.emit('onReady', {
                 id: socket.id
             });
+
+            socket.emit('onRefreshClient', await refresh(io));
         });
 
         socket.on('messageSend', (message: Message) => {
@@ -84,119 +72,46 @@ function run(port: number = 3000): void {
         });
 
         socket.on('refresh', async () => {
-            socket.emit('onRefreshClient', await refresh(io, socket));
+            socket.emit('onRefreshClient', await refresh(io));
         });
 
         socket.on('joinRoom', async (roomName: string) => {
             await joinSocket(socket, roomName);
-            const eventMessage: EventMessage = { id: socket.id, rooms: [roomName] }
-            await io.serverSideEmit('onJoinRoom', eventMessage);
-            setTimeout(async () => { io.emit('onRefreshClient', await refresh(io, socket)); }, 200);
+            io.emit('onRefreshClient', await refresh(io));
         });
 
         socket.on('leaveRoom', async (roomName: string) =>  {
             await leaveSocket(socket, roomName);
-            const eventMessage: EventMessage = { id: socket.id, rooms: [roomName] }
-            await io.serverSideEmit('onLeaveRoom', eventMessage);
-            setTimeout(async () => { io.emit('onRefreshClient', await refresh(io, socket)); }, 200);
+            io.emit('onRefreshClient', await refresh(io));
         });
 
         socket.on('disconnect', async () => {
-            if (clientRoom.has(socket.id)) {
-                let rooms:any = clientRoom.get(socket.id);
-                rooms = Array.from(rooms.keys());
-                for(const room of rooms) {
-                    leaveChannel(socket.id, room);
-                    delClient(socket.id, room);
-                }
-
-                const eventMessage: EventMessage = { id: socket.id, rooms: rooms }
-                await io.serverSideEmit('onLeaveRoom', eventMessage);
-                setTimeout(async () => { io.emit('onRefreshClient', await refresh(io, socket)); }, 200);
-            }
+            io.emit('onRefreshClient', await refresh(io));
         });
     });
 }
 
 async function joinSocket(socket: Socket, roomName: string) {
     await socket.join('Room:' + roomName);
-    putClient(socket.id, 'Room:' + roomName);
-    joinChannel(socket.id, 'Room:' + roomName);
 }
 
 async function leaveSocket(socket: Socket, roomName: string) {
     await socket.leave(roomName);
-    delClient(socket.id, roomName);
-    leaveChannel(socket.id, roomName);
 }
 
-function putClient(id: string, roomName: string) {
-    if(!clientRoom.has(id)) {
-        clientRoom.set(id, new Map<string, string>());
-    }
-
-    const rooms:Map<string, string> | undefined = clientRoom.get(id);
-    if (rooms && !rooms.has(roomName)) {
-        rooms.set(roomName, roomName);
-    }
-}
-
-function delClient(id: string, roomName: string) {
-    if(!clientRoom.has(id)) {
-        return;
-    }
-
-    const rooms:Map<string, string> | undefined = clientRoom.get(id);
-    if (rooms && rooms.has(roomName)) {
-        rooms.delete(roomName);
-
-        if (rooms.size == 0) {
-            clientRoom.delete(id);
-        }
-    }
-}
-
-function joinChannel(id: string, roomName: string) {
-    if(!channels.has(roomName)) {
-        channels.set(roomName, new Map<string, string>());
-    }
-
-    const clients:Map<string, string> | undefined = channels.get(roomName);
-    if (clients && !clients.has(id)) {
-        clients.set(id, id);
-    }
-}
-
-function leaveChannel(id: string, roomName: string) {
-    if(!channels.has(roomName)) {
-        return;
-    }
-
-    const clients:Map<string, string> | undefined = channels.get(roomName);
-    if (clients && clients.has(id)) {
-        clients.delete(id);
-
-        if (clients.size == 0) {
-            channels.delete(roomName);
-        }
-    }
-}
-
-async function refresh(io: any, socket: any): Promise<RefreshMessage> {
+async function refresh(io: any): Promise<RefreshMessage> {
     const clientsByRoom: clientsByRoomType[] = [];
     const allRooms:string[] = Array.from(await io.of('/').adapter.allRooms());
     const rooms = allRooms.filter((room: string) => { return room.indexOf("Room:") > -1 });
 
     if (rooms.length > 0) {
         for (const room of rooms) {
-            if (channels.has(room)) {
-                const clientList = channels.get(room) || new Map<string, string>();
-                const clients:string[] = Array.from(clientList.keys());
-                clientsByRoom.push({
-                    key: room,
-                    clients: clients
-                });
-            }
+            const clientList = await io.of('/').adapter.sockets(new Set([room]));
+            const clients:string[] = Array.from(clientList);
+            clientsByRoom.push({
+                key: room,
+                clients: clients
+            });
         }
     }
 
